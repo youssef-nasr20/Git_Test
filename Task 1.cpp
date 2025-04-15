991557b80f068f92460de0955f6f7b56bb0438cd
youssef-nasr20 <147179580+youssef-nasr20@users.noreply.github.com>
2025-04-15 14:43:37 +0200

    Add files via upload

---
+++ Task 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    char operation;
+        cout<< "Enter the first number ";
+        cin>> num1;
+        cout<< "Enter the second number ";
+        cin>> num2;
+        cout<< "Enter the operation ";
+        cin>> operation;
+        
+    if (operation == '+') {
+        result = num1 + num2;
+        }
+    else if (operation == '-') {
+        result = num1 - num2;
+        }
+    else if (operation == '*') {
+        result = num1 * num2;
+        }
+    else if (operation == '/') {
+        result = num1 / num2;
+        }
+    else{
+        cout<<"Incorrect operation"<< endl;
+        }
+    
+    cout << "The result is :  " << result << endl;
+
+    return 0;
+
+}
